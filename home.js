8abb9ca4a33a8fd7c3fd8a2dfe747f4e9e24e0ff
Ana Hatsue <anahatsue.dev@outlook.com>
2023-01-08 22:48:38 -0300

    Adicionado tela de saudação e tipos de pokemons.

--- home.js
+++ home.js
@@ -1,7 +1,12 @@
 var arrayObjects = [];
 var pokemonCard = document.querySelector(".pokemons");
+
 var form = document.querySelector(".search");
 var input = document.querySelector(".search-bar");
+
+var displayCard = pokemonCard;
+var displayWelcome = document.querySelector(".welcome-container");
+var displayNotFound = document.querySelector(".pokemon-nfound");
 
 async function fetchPokemonById(id) {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
@@ -11,7 +16,7 @@ async function fetchPokemonById(id) {
 }
 
 async function fetchAllPokemons() {
-    var numberPokemons = 152;
+    var numberPokemons = 905;
 
     for (let index = 1; index <= numberPokemons; index++) {
         fetchPokemonById(index).then( (dataPromise) => {
@@ -28,16 +33,39 @@ function renderPokemonHome() {
 
     pokemonCard.innerHTML = "";
 
+    var findAny = false;
+
     arrayObjects.forEach(element => {
         if(element.name.includes(input.value.toLowerCase()) && input.value.length > 1) {
             renderPokemonCard(element)
+            findAny = true;
         }
     });
+
+    displaysControl(findAny);
+    
+}
+
+function displaysControl(findAny) {
+    if(findAny) {
+        displayCard.style.display = "flex";
+        displayNotFound.style.display = "none";
+        displayWelcome.style.display = "none";
+    } else {
+        displayCard.style.display = "none";
+        if(input.value.length > 2) {
+            displayNotFound.style.display = "flex";
+            displayWelcome.style.display = "none";
+        } else {
+            displayWelcome.style.display = "flex";
+            displayNotFound.style.display = "none";
+        }
+    } 
 }
 
 function renderPokemonCard(element) {
     pokemonCard.innerHTML += 
-        `<div class='home-poke-card'> 
+        `<div class='home-poke-card' onclick='clickPokemon(${element.id})'> 
             <div class='home-poke-card-id'> 
                     ${element.id} 
                 </div> 
@@ -46,12 +74,17 @@ function renderPokemonCard(element) {
             </div>
             <div class='home-poke-card-data'>
                 <div class='home-poke-card-name'> 
-                ${element.name} 
+                    ${element.name} 
                 </div>
                 
             </div> 
             
         <div>`;
+}
+
+function clickPokemon(pokemonId){
+    localStorage.setItem("pokemonId", pokemonId);
+    window.open("card.html","_self");
 }
 
 window.onload = () => {
--- script.js
+++ script.js
@@ -12,6 +12,27 @@ const input = document.querySelector(".search-bar");
 const displayPokemon = document.querySelector(".card-box");
 const notDisplayPokemon = document.querySelector(".card-box-nfound");
 
+const types = {
+	fire: {color: "#f42", translate: "fogo"},
+	grass: {color: "#7c5", translate: "planta"},
+	electric: {color: "#fc3", translate: "elétrico"},
+	water: {color: "#39f", translate: "água"},
+	ground: {color: "#db5", translate: "terra"},
+	rock: {color: "#ba6", translate: "pedra"},
+	fairy: {color: "#e9e", translate: "fada"},
+	poison: {color: "#a59", translate: "veneno"},
+	bug: {color: "#ab2", translate: "inseto"},
+	dragon: {color: "#76e", translate: "dragão"},
+	psychic: {color: "#f59", translate: "psíquico"},
+	flying: {color: "#89f", translate: "voador"},
+	fighting: {color: "#b54", translate: "lutador"},
+	normal: {color: "#aa9", translate: "normal"},
+    ghost: {color: "#66b", translate: "fantasma"},
+    steel: {color: "#aab", translate: "metal"},
+    ice: {color: "#6cf", translate: "gelo"},
+    dark: {color: "#754", translate: "noturno"}
+};
+
 // Para buscar os dados da API
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -19,7 +40,7 @@ const fetchPokemon = async (pokemon) => {
     if(APIResponse.status == 200) {
         const data = await APIResponse.json();
         return data;
-    }
+    } 
 }
 
 // Para pegar os dados do pokemon pesquisado
@@ -37,10 +58,21 @@ const renderPokemon = async (pokemon) => {
         pokemonId.innerHTML = "nº " + data.id;
         pokemonHeight.innerHTML = data.height/10 + "m";
         pokemonWeight.innerHTML = data.weight/10 + "kg";
-        pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+
+        const imgBackup = data.sprites.front_default;
+        const animatedImg = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+        pokemonImage.src = animatedImg || imgBackup;
+
         data.types.forEach(element => {
-            pokemonType.innerHTML += "<div class='one-type-pokemon'>" + element.type.name + "</div>";
+            var typeName = element.type.name;
+            var translate = types[typeName]["translate"];
+            var color =  types[typeName]["color"];
+
+            pokemonType.innerHTML += 
+            `<div class='one-type-pokemon' style="background-color:${color}">${translate}</div>`;
         });
+
+        localStorage.setItem("pokemonId", data.id);
     }
 }
 
@@ -64,3 +96,31 @@ function clearInput () {
 function cardTab() {
     window.open("card.html")
 }
+
+//Função para fazer loading da página assim que abrir
+window.onload = () => {
+    renderPokemon(localStorage.getItem("pokemonId"));
+}
+
+const buttonPrev = document.querySelector("#btn-previous");
+const buttonNext = document.querySelector("#btn-next");
+
+buttonPrev.addEventListener("click", event => {
+    event.preventDefault();
+    const storageId = localStorage.getItem("pokemonId");
+    const intStorageId = parseInt(storageId);
+    if(intStorageId > 1) {
+        renderPokemon(intStorageId - 1);
+    } else {
+
+    }
+})
+
+buttonNext.addEventListener("click", event => {
+    event.preventDefault();
+    const storageId = localStorage.getItem("pokemonId");
+    const intStorageId = parseInt(storageId);
+    if(intStorageId < 906) {
+        renderPokemon(intStorageId + 1);
+    }
+})
